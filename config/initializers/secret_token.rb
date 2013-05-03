8c5e8617d51aa5cd027ab0faf1fbe75797c82622
Nathanial Allan <nathanial.allan@gmail.com>
2013-05-03 17:25:01 +0800

    refuckulate

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -2,11 +2,6 @@
 
 # Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
-
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-# You can use `rake secret` to generate a secure secret key.
-
-# Make sure your secret_key_base is kept private
-# if you're sharing your code publicly.
-Nalsoft::Application.config.secret_key_base = 'afafffd57ac4d34f0f96a26bd1aefe767d964dfdb8f79a2f8d3d5e66ad52c984038ce212c881bc6639c797790a4355be454f73b29b794ca34dce54a8592525c6'
+Nalsoft::Application.config.secret_token = '2f89e3dbc21b5360ab57b7d1191825433332bbc23c6a2f00f17aec5922e7662874c3a9fefafca91cd642e58a8ec4abd3cea70c2d1599af5a0a3e01d08a84f052'
