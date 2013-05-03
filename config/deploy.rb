f412fd7ffe0a77db06364deea9e6092752c23ce6
Nathanial Allan <nathanial.allan@gmail.com>
2013-05-03 20:38:20 +0800

    checkup

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,6 @@
+require "bundler/capistrano"
 default_run_options[:pty] = true
+
 set :application, "nalsoft"
 set :repository,  "ssh://git@github.com/nallan/nalsoft"
 
@@ -10,22 +12,23 @@ set :deploy_to, "/var/www"
 
 server "198.199.107.176", :app, :web, :db, :primary => true
 
-#role :web, "198.199.107.176"                          # Your HTTP server, Apache/etc
-#role :app, "198.199.107.176"                          # This may be the same as your `Web` server
-#role :db,  "198.199.107.176", :primary => true # This is where Rails migrations will run
-#role :db,  "your slave db-server here"
+
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
+set :keep_releases, 5
+after "deploy:restart", "deploy:cleanup"
+after 'deploy:update_code', 'deploy:migrate'
+
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
 # If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+ namespace :deploy do
+   task :start do ; end
+   task :stop do ; end
+   task :restart, :roles => :app, :except => { :no_release => true } do
+     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+   end
+ end
