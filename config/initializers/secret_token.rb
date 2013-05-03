537fe9f4094b31fab3154df8cd39da7c5e12a6ed
Nathanial Allan <nathanial.allan@gmail.com>
2013-05-03 13:49:33 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nalsoft::Application.config.secret_key_base = 'afafffd57ac4d34f0f96a26bd1aefe767d964dfdb8f79a2f8d3d5e66ad52c984038ce212c881bc6639c797790a4355be454f73b29b794ca34dce54a8592525c6'
